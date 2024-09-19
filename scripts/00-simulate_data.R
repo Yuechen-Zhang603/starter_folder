cae7df92c449798f646f14a8b8abcad0ae61fad8
Yuechen-Zhang603 <yuechen.zhang@mail.utoronto.ca>
2024-09-19 17:41:29 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,9 +1,8 @@
-#### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
+# Purpose: Simulates marrige 
+# Author: Yuechen Zhang 
+# Date: 19 Sep 2024 
+# Contact: yuechen.zhang@mail.utoronto.ca
+# License: Uoft
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
@@ -13,7 +12,26 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(304)
+start_date <- as.Date("2018-01-01")
+end_date <- as.Date("2023-12-31")
+
+number_of_dates <- 100
+
+data <- 
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_marrige = rpois(n = number_of_dates, lambda = 15)
+  )
+
+write.csv(data, file = "data/raw_data/simulated.csv")
 
 
 
